499fa706aa34c5d469191225a4774399a610f7a6
djoeyg <greend3@oregonstate.edu>
2021-10-25 20:10:57 -0700

    Initial commit with template files

---
+++ movies-react-ui/src/pages/HomePage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MovieList from '../components/MovieList';
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function HomePage({ setMovieToEdit }) {
+
+    const [movies, setMovies] = useState([]);
+    const history = useHistory();
+
+    const onDelete = async _id => {
+        const response = await fetch(`/movies/${_id}`, { method: 'DELETE' });
+        if (response.status === 204) {
+            setMovies(movies.filter(m => m._id !== _id));
+        } else {
+            console.error(`Failed to delete movie with _id = ${_id}, status code = ${response.status}`);
+        }
+    };
+
+    const onEdit = movie => {
+        setMovieToEdit(movie);
+        history.push("/edit-movie");
+    }
+
+    const loadMovies = async () => {
+        const response = await fetch('/movies');
+        const data = await response.json();
+        setMovies(data);
+    }
+
+    useEffect(() => {
+        loadMovies();
+    }, []);
+
+    return (
+        <>
+            <h2>List of Movies</h2>
+            <MovieList movies={movies} onDelete={onDelete} onEdit={onEdit}></MovieList>
+            <Link to="/add-movie">Add a movie</Link>
+        </>
+    );
+}
+
+export default HomePage;
